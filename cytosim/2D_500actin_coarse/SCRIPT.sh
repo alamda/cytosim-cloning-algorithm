cd66e7134c6e648b5460f56aa747cab8dd6257aa
Alexandra Lamtyugina <alexandral@midway2-login1.rcc.local>
2020-07-15 18:03:09 -0500

    added a couple of descriptive comments to script

--- cytosim/2D_500actin_coarse/SCRIPT.sh
+++ cytosim/2D_500actin_coarse/SCRIPT.sh
@@ -3,5 +3,5 @@
 # use template to create individual config file
 ./preconfig.py config.cym.tpl
 
-# copy *.cym files into individual directories 
+# copy *.cym files and necessary executables into individual directories and submit simulations with sbatch
 ./run_sims.sh
