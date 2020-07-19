83f0e5d1ca8dae1982724f0695818fd34dd678b1
Alexandra Lamtyugina <alexandral@uchicago.edu>
2020-07-18 19:31:43 -0500

    wrote a good chunk of the parser, which currently does not compile. current implementation is inefficient for large data files as it reads the whole file at once instead of a couple of frames at a time. may want to revist at a later point, though right now the current version should be serviceable.

--- parse_output.cpp
+++ parse_output.cpp
@@ -1,27 +1,168 @@
-/*
- * File I began writing to parse cytosim output files.
- *
- * Will try to avoid parsing output files by integrating cloning
- * with Cytosim code.
- *
- */
-
-
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 #include <cstdio>
+#include "parse_output.h"
 
-// Define a constant for the max buffer size
-constexpr int maxstr = 1024 ;
+// Used as reference: https://thispointer.com/c-how-to-read-a-file-line-by-line-into-a-vector/
+bool get_file_contents( std::string fileName, std::vector<std::string> & vectorOfFileLines )
+{
+	// open file
+	std::ifstream file(fileName.c_str());
+
+	// Check if object is valid
+	if(!file)
+	{
+		std::cerr << "Cannot open the File : " << fileName << std::endl;
+		return false;
+	}
+
+	// Define string variable
+	std::string str ;
+
+	// Read the next line from File untill it reaches the end.
+	while (std::getline(file, str))
+	{
+		// Line contains string of length > 0 then save it in vector
+		if(str.size() > 0)
+			vectorOfFileLines.push_back(str);
+	}
+
+	//Close The File
+	file.close();
+
+	return true;
+}
+
+struct Frame
+{
+	int frameNumber ;
+	const std::string frameStr = "frame";
+
+	double timeStamp ;
+	const std::string timeStr = "time";
+
+	std::string reportCommand ;
+	const std::string reportStr = "report";
+
+	std::string dataCategories ;
+	const std::string categoriesStr = "class";
+
+	std::vector <std::string> dataLines ;
+};
+
+void process_frame(std::vector <std::vector> & vectorOfFrameContent, std::vector <Frame> & vectorOfFrameObjects)
+{
+	Frame frame ;
+
+	for(std::string & line : vectorOfFrameContent)
+	{
+
+
+		if (line.find(frame.frameStr) != std::string::npos)
+		{
+			printf("%c", line.c_str());// Extract frame number int
+		}
+		// else if (/* condition */)
+		// {
+		// 	// Extract timestamp double
+		// }
+		// else if ()
+		// {
+		// 	// Extract string with report command used to generate data file
+		// }
+		// else if ()
+		// {
+		// 	// Extract string with category names of data in file
+		// }
+		// else if ()
+		// {
+		// 	// Extract vector of strings of data lines for the frame
+		// }
+
+
+	}
+}
+
+
+void split_frames(std::vector<std::string> & vectorOfFileLines,
+				  std::vector <std::vector <std::string> > & vectorOfFrames,
+				  std::vector <Frame> & vectorOfFrameObjects,
+				  std::string const startSubstring,
+				  std::string const endString )
+{
+	// define vector for storing content of a single frames
+	std::vector <std::string> vectorOfFrameContent;
+
+	// Iterate over each line in the file
+	for(std::string & line : vectorOfFileLines)
+	{
+		// check if the line contains the string denoting the start of the frame
+		std::size_t found = line.find(startSubstring);
+
+		// if start substring is found in the line
+		if (found != std::string::npos)
+		{
+			// empty out the vector of frame content
+			vectorOfFrameContent.clear() ;
+
+			printf("start of frame\n");
+		}
+
+		vectorOfFrameContent.push_back(line);
+
+		// check if the line contains the string denoting the end of the frame
+		found = line.find(endString);
+
+		if (found != std::string::npos)
+		{
+			vectorOfFrames.push_back(vectorOfFrameContent);
+
+			printf("end of frame, number of lines for frame is %d\n\n",
+					vectorOfFrameContent.size());
+
+			process_frame(vectorOfFrameContent, vectorOfFrameObjects);
+		}
+	}
+
+	printf("end of file parsing, number of frames is %d\n\n", vectorOfFrames.size());
+
+	printf("%c\n", *vectorOfFrames.end());
+
+}
+
+
+
 
 int main()
 {
-	char buffer[maxstr] ; // define a buffer
+	// define vector for storing lines extracted from file
+	std::vector <std::string> vectorOfFileLines;
 
-	FILE * file_reference = fopen("link.txt", "r");
+	// define vector for storing frames which store lines corresponding to the frames
+	std::vector <std::vector <std::string> >  vectorOfFrames;
 
-	while(fgets( buffer, maxstr, file_reference ))
+	std::vector <Frame> vectorOfFrameObjects ;
+
+
+	// Get the contents of file in a vector
+	// vectorOfLines is modified by the function because one of the arguments
+	// is actually defined to be a reference to vectorOfLines ???
+	bool result = get_file_contents("link.txt", vectorOfFileLines);
+
+	// Specify strings at start and end of file
+	const std::string startSubstring = "% frame";
+	const std::string endString = "% end";
+
+	// Check that the file exists/is valid
+	if(result)
 	{
-		fputs(buffer, stdout);
+		// Pass a pointer to vectorOfFrames, everything else unchanged
+		split_frames(vectorOfFileLines, vectorOfFrames, vectorOfFrameObjects, startSubstring, endString);
 	}
 
-	fclose(file_reference);
+
+
+	return 0;
 }
