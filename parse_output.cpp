523ae2e25df5187dfc18b5dcf01a90d9270dbce2
Alexandra Lamtyugina <alexandral@uchicago.edu>
2020-07-20 14:44:04 -0500

    cleaned up main() and moved things to the revived get_file_contsnts. split_frames() was retired. next: process strings from data file to extract ints and floats for data

--- parse_output.cpp
+++ parse_output.cpp
@@ -6,37 +6,6 @@
 #include <memory> // For unique_ptr
 #include "parse_output.h"
 
-
-// Used as reference: https://thispointer.com/c-how-to-read-a-file-line-by-line-into-a-vector/
-bool get_file_contents( std::string fileName, std::vector<std::string> & vectorOfFileLines )
-{
-	// open file
-	std::ifstream file(fileName.c_str());
-
-	// Check if object is valid
-	if(!file)
-	{
-		std::cerr << "Cannot open the File : " << fileName << std::endl;
-		return false;
-	}
-
-	// Define string variable
-	std::string str ;
-
-	// Read the next line from File untill it reaches the end.
-	while (std::getline(file, str))
-	{
-		// Line contains string of length > 0 then save it in vector
-		if(str.size() > 0)
-			vectorOfFileLines.push_back(str);
-	}
-
-	//Close The File
-	file.close();
-
-	return true;
-}
-
 struct Frame
 {
 	int frameNumber ;
@@ -87,92 +56,26 @@ void process_frame(std::vector <std::string> & vectorOfFrameContent, std::vector
 	}
 }
 
-
-void split_frames(std::vector<std::string> & vectorOfFileLines,
-				  std::vector <std::vector <std::string> > & vectorOfFrames,
-				  std::vector <Frame> & vectorOfFrameObjects,
-				  std::string const startSubstring,
-				  std::string const endString )
+// Used as reference: https://thispointer.com/c-how-to-read-a-file-line-by-line-into-a-vector/
+ void get_file_contents( std::string fileName,
+						std::string const startSubstring,
+						std::string const endString,
+						std::vector <std::vector <std::string> > & vectorOfFrames,
+						std::vector <Frame> & vectorOfFrameObjects )
 {
-	// define vector for storing content of a single frames
-	std::vector <std::string> vectorOfFrameContent;
-
-	// Iterate over each line in the file
-	for(std::string & line : vectorOfFileLines)
-	{
-		// check if the line contains the string denoting the start of the frame
-		std::size_t found = line.find(startSubstring);
-
-		// if start substring is found in the line
-		if (found != std::string::npos)
-		{
-			// empty out the vector of frame content
-			vectorOfFrameContent.clear() ;
-
-			printf("start of frame\n");
-		}
-
-		vectorOfFrameContent.push_back(line);
-
-		// check if the line contains the string denoting the end of the frame
-		found = line.find(endString);
-
-		if (found != std::string::npos)
-		{
-			vectorOfFrames.push_back(vectorOfFrameContent);
-
-			printf("end of frame, number of lines for frame is %d\n\n",
-					vectorOfFrameContent.size());
-
-			process_frame(vectorOfFrameContent, vectorOfFrameObjects);
-		}
-	}
-
-	printf("end of file parsing, number of frames is %d\n\n", vectorOfFrames.size());
-
-	printf("%c\n", *vectorOfFrames.end());
-
-}
-
-
-
-
-int main()
-{
-	// Name of data file to be read:
-	const std::string fileName = "link.txt";
-	const std::string startSubstring = "% frame";
-	const std::string endString = "% end";
-
-	// define vector for storing lines extracted from file
-	std::vector <std::string> vectorOfFileLines;
-
-	// define vector for storing frames which store lines corresponding to the frames
-	std::vector <std::vector <std::string> >  vectorOfFrames;
-
-	std::vector <Frame> vectorOfFrameObjects ;
-
-	//For reading large files:
-	// Source: https://stackoverflow.com/questions/34751873/how-to-read-huge-file-in-c
-
-
+	// open file
 	std::ifstream dataFile(fileName.c_str());
 
-
-	if(!dataFile)
-	{
-		std::cerr << "Cannot open the File : " << fileName << std::endl;
-		return false;
-	}
+	// define vector for storing content of a single frames
+	std::vector <std::string> vectorOfFrameContent;
 
 	while (dataFile)
 	{
 		// Define string variable
 		std::string line ;
+		int frameNumber = 0;
 
-		// define vector for storing content of a single frames
-		std::vector <std::string> vectorOfFrameContent;
-
+		// Read line by line and sort into frames
 		while (std::getline(dataFile, line))
 		{
 			// Line contains string of length > 0 then save it in vector
@@ -187,7 +90,7 @@ int main()
 					// empty out the vector of frame content
 					vectorOfFrameContent.clear() ;
 
-					printf("start of frame\n");
+					printf("start of frame %d\n", frameNumber);
 				}
 
 				vectorOfFrameContent.push_back(line);
@@ -199,34 +102,52 @@ int main()
 				{
 					vectorOfFrames.push_back(vectorOfFrameContent);
 
-					printf("end of frame, number of lines for frame is %d\n\n",
-							vectorOfFrameContent.size());
+					printf("end of frame %d, number of lines for this frame is %d\n\n", frameNumber, vectorOfFrameContent.size());
+
+					frameNumber++ ;
 
 					process_frame(vectorOfFrameContent, vectorOfFrameObjects);
+
+					if (frameNumber > 1)
+					{
+						// do calculations on two adjacent frames
+					}
+
 				}
 			}
+
+
 		}
 
-		split_frames(vectorOfFileLines, vectorOfFrames, vectorOfFrameObjects, startSubstring, endString);
+	printf("Total number of frames: %d", frameNumber);
+
 	}
 
-
-	// Get the contents of file in a vector
-	// vectorOfLines is modified by the function because one of the arguments
-	// is actually defined to be a reference to vectorOfLines ???
-	// bool result = get_file_contents("link.txt", vectorOfFileLines);
-
-	// Specify strings at start and end of file
+	//Close The File
+	dataFile.close();
+}
 
 
-	// Check that the file exists/is valid
-	// if(result)
-	// {
-	// 	// Pass a pointer to vectorOfFrames, everything else unchanged
-	// 	split_frames(vectorOfFileLines, vectorOfFrames, vectorOfFrameObjects, startSubstring, endString);
-	// }
+int main()
+{
+	// Name of data file to be read:
+	const std::string fileName = "link.txt";
+	const std::string startSubstring = "% frame";
+	const std::string endString = "% end";
 
+	// define vector for storing lines extracted from file
+	// std::vector <std::string> vectorOfFileLines;
 
+	// define vector for storing frames which store lines corresponding to the frames
+	std::vector <std::vector <std::string> >  vectorOfFrames;
+
+	std::vector <Frame> vectorOfFrameObjects ;
+
+	//For reading large files:
+	// Source: https://stackoverflow.com/questions/34751873/how-to-read-huge-file-in-c
+
+	get_file_contents(fileName, startSubstring, endString, vectorOfFrames,
+					  vectorOfFrameObjects);
 
 	return 0;
 }
