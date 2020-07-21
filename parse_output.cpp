83ff571424bd932bda23ebec9a59c61c5e729e0a
Alexandra Lamtyugina <alexandral@uchicago.edu>
2020-07-21 10:16:51 -0500

    got regex to work (kind of) in the process_line function.  need to think about a better/faster way of parsing strings of file lines to determine what they are.  perhaps only check for beginning and end lines, and everything else is in the same order?

--- parse_output.cpp
+++ parse_output.cpp
@@ -3,7 +3,7 @@
 #include <string>
 #include <vector>
 #include <cstdio>
-#include <memory> // For unique_ptr
+#include <regex>
 #include "parse_output.h"
 
 struct Frame
@@ -11,7 +11,7 @@ struct Frame
 	int frameNumber ;
 	const std::string frameStr = "frame";
 
-	double timeStamp ;
+	float timeStamp ;
 	const std::string timeStr = "time";
 
 	std::string reportCommand ;
@@ -23,37 +23,44 @@ struct Frame
 	std::vector <std::string> dataLines ;
 };
 
-void process_frame(std::vector <std::string> & vectorOfFrameContent, std::vector <Frame> & vectorOfFrameObjects)
+void process_line(std::string line, Frame & frame)
 {
-	Frame frame ;
+	std::smatch match ;
 
-	for(std::string & line : vectorOfFrameContent)
+	if (line.find(frame.frameStr) != std::string::npos)
 	{
+		std::regex rgx("[0-9]+");
 
-
-		if (line.find(frame.frameStr) != std::string::npos)
+		if (std::regex_search(line, match, rgx))
 		{
-			printf("%c", line.c_str());// Extract frame number int
+			frame.frameNumber =  std::stoi(match.str(0));
 		}
-		// else if (/* condition */)
-		// {
-		// 	// Extract timestamp double
-		// }
-		// else if ()
-		// {
-		// 	// Extract string with report command used to generate data file
-		// }
-		// else if ()
-		// {
-		// 	// Extract string with category names of data in file
-		// }
-		// else if ()
-		// {
-		// 	// Extract vector of strings of data lines for the frame
-		// }
-
-
 	}
+	else if (line.find(frame.timeStr) != std::string::npos)
+	{
+		std::regex rgx("[0-9]+.[0-9]+");
+
+		if (std::regex_search(line, match, rgx))
+		{
+			frame.timeStamp =  std::stof(match.str(0));
+			printf("%s\n", line);
+			printf("%d\n", frame.timeStamp);
+		}
+	}
+	// else if ()
+	// {
+	// 	// Extract string with report command used to generate data file
+	// }
+	// else if ()
+	// {
+	// 	// Extract string with category names of data in file
+	// }
+	// else if ()
+	// {
+	// 	// Extract vector of strings of data lines for the frame
+	// }
+
+
 }
 
 // Used as reference: https://thispointer.com/c-how-to-read-a-file-line-by-line-into-a-vector/
@@ -78,55 +85,52 @@ void process_frame(std::vector <std::string> & vectorOfFrameContent, std::vector
 		// Read line by line and sort into frames
 		while (std::getline(dataFile, line))
 		{
-			// Line contains string of length > 0 then save it in vector
-			if(line.size() > 0)
+			std::size_t found_end ;
+			std::size_t found_start ;
+
+			found_end = line.find(endString);
+
+			// If the line does not match endString
+			if(found_end = std::string::npos)
 			{
 				// check if the line contains the string denoting the start of the frame
-				std::size_t found = line.find(startSubstring);
+				found_start = line.find(startSubstring);
 
 				// if start substring is found in the line
-				if (found != std::string::npos)
+				if (found_start != std::string::npos)
 				{
-					// empty out the vector of frame content
-					vectorOfFrameContent.clear() ;
+					// Create new frame object
+					Frame frame; // needs to be available to other scopes
+					//would like to initialize object in this scope tho
 
-					printf("start of frame %d\n", frameNumber);
+					printf("start of frame %d, frame object created\n", frameNumber);
 				}
 
-				vectorOfFrameContent.push_back(line);
+				process_line(line, frame);
 
-				// check if the line contains the string denoting the end of the frame
-				found = line.find(endString);
 
-				if (found != std::string::npos)
+				if (found_end != std::string::npos)
 				{
-					vectorOfFrames.push_back(vectorOfFrameContent);
+					// will remove vector of frames
+					vectorOfFrameObjects.push_back(frame);
 
-					printf("end of frame %d, number of lines for this frame is %d\n\n", frameNumber, vectorOfFrameContent.size());
+					printf("end of frame %d, pushing back frame objects to vector", frameNumber);
 
 					frameNumber++ ;
-
-					process_frame(vectorOfFrameContent, vectorOfFrameObjects);
 
 					if (frameNumber > 1)
 					{
 						// do calculations on two adjacent frames
 					}
-
 				}
 			}
-
-
 		}
-
-	printf("Total number of frames: %d", frameNumber);
-
+	printf("Total number of frames: %d\n", frameNumber);
 	}
 
 	//Close The File
 	dataFile.close();
 }
-
 
 int main()
 {
@@ -135,16 +139,11 @@ int main()
 	const std::string startSubstring = "% frame";
 	const std::string endString = "% end";
 
-	// define vector for storing lines extracted from file
-	// std::vector <std::string> vectorOfFileLines;
-
 	// define vector for storing frames which store lines corresponding to the frames
+	// will eventually remove vector of frames  discard frame data after doing mafs
 	std::vector <std::vector <std::string> >  vectorOfFrames;
 
 	std::vector <Frame> vectorOfFrameObjects ;
-
-	//For reading large files:
-	// Source: https://stackoverflow.com/questions/34751873/how-to-read-huge-file-in-c
 
 	get_file_contents(fileName, startSubstring, endString, vectorOfFrames,
 					  vectorOfFrameObjects);
